067e22617b31dfedd9fda62b6d5b0eb88780e5d2
Joanna Pompeo <joannapompeo@gmail.com>
2020-10-06 14:35:26 -0400

    Connecting app to store

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
@@ -7,22 +9,24 @@ import { UserList } from './components/UserList';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route
-                exact
-                path="/"
-                render={(props) => <App userType='admin' username='h4x0r' />}
-            />
-        </Switch>
-        <Switch>
-            <Route
-                exact
-                path='/userList'
-                render={(props) => <UserList {...props} />}
-            />
-        </Switch>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    render={(props) => <App userType='admin' username='h4x0r' />}
+                />
+            </Switch>
+            <Switch>
+                <Route
+                    exact
+                    path='/userList'
+                    render={(props) => <UserList {...props} />}
+                />
+            </Switch>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 
